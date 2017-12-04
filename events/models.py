e93defec368595131cf0979a85f79eb1144db4b2
Owens O'Brien <owensobrien@gmail.com>
2017-12-04 15:50:19 +0700

    ignoring

--- events/models.py
+++ events/models.py
@@ -3,13 +3,16 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Shows(models.Model):
+class CurrentShows(models.Model):
     created_by = models.ForeignKey('auth.User')
     address = models.CharField(max_length=255, blank=True)
     venue_name = models.CharField(max_length=255)
     created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(blank=True, null=True)
     startDate = models.DateField(blank=True, null=True)
     endDate = models.DateField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
     show_image = models.ImageField(upload_to='shows/', null=True)
+
+class ArchivedShows(models.Model):
+    created_by = models.ForeignKey('auth.User')
+    venue_name = models.CharField(max_length=255)
